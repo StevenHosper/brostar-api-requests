0bc816cf0a6f3fbd6fe558fc3a2399f3fd05a7b0
StevenHosper <steven.hosper@ziggo.nl>
2025-11-25 17:01:43 +0100

    Added GLD Deletion example

---
+++ src/brostar_api_requests/examples.py
@@ -0,0 +1,10 @@
+def delete_gld_observations_gelderland():
+    """An example of how one could delete the observations of a GLD ID."""
+    import datetime
+
+    from src.brostar_api_requests.brostar_api_requests import GLDCorrecter
+
+    bro_id = "GLD000000046735"
+    correcter = GLDCorrecter(bro_id)
+    correcter.set_project_number("5459")
+    correcter.delete_observations(start_date=datetime.datetime(2023, 8, 1), lower_then=True)
